cf3b94ec28c716b2fa8932fe7ae291be1de39d27
jenifer-brown <ID+USERNAME@users.noreply.github.com>
2022-11-17 23:56:02 -0500

    Connected Movie Api

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,19 +1,47 @@
 import SingleResult from "./SingleResult"
 
-const mockData = [
-    {name: "A"},
-    {name: "B"},
-    {name: "C"}
-]
+const mockData = [];
 
 export default function Results(props) {
+
+    const [resultsArray, setResultsArray] = React.useState([]);
+    const [render, setRender] = React.useState(false);
+
+    React.useEffect(() => {
+        const searchResults = "";
+        fetch(
+            "https://api.themoviedb.org/3/list/2910?api_key=d8cb9be625954d1f4e586efec42d4e79&language=en-US"
+        )
+          .then((result) => result.json())
+          .then((output) => {
+            
+            if (!output.status_code) {
+              for (let i = output.items.length - 1; i >= 0 ; i--) {
+                mockData.push(output.items[i]);
+              }
+            }
+            console.log(mockData);
+            const addResults = mockData.map((entry) => 
+            <SingleResult 
+                title={entry.original_title}
+                img={`https://image.tmdb.org/t/p/w500${entry.poster_path}`}
+                year={entry.release_date.substring(0, 4)}
+                summary={entry.overview}
+                movieId={entry.id}
+                />);
+            setResultsArray(addResults);
+          })
+          .catch((err) => {
+            searchResults = err.status_code + " ERROR: " + err.status_message;
+            mockData.push(searchResults);
+            
+          });
+    }, []);
 
     return (
 
         <div className="results">
-            {mockData.map((entry) => {
-                return <SingleResult name={entry.name}/>
-            })}
+            {resultsArray}
         </div>
     )
 }
--- src/components/results/SingleResult.jsx
+++ src/components/results/SingleResult.jsx
@@ -1,6 +1,13 @@
 
 export default function SingleResult(props) {
+    const {title, img, year, summary, movieId } = props;
+    const resultId = `movie-${movieId}`;
     return (
-        <p> this is result #{props.name}</p>
+        <div id={resultId} className="singleResult">
+            <h1>{title}</h1>
+            <h2>{year}</h2>
+            <img src={img}/>
+            <p>{summary}</p>
+        </div>
     )
 }
