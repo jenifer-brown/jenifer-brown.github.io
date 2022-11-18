42a26eb2c920788e1fb67c4c75700e59d35d9cc4
jenifer-brown <ID+USERNAME@users.noreply.github.com>
2022-11-17 20:12:10 -0500

    Added scrollable div

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import App from './App'
 import './index.css'
 
 
-function Maker() {
-  return <p> this is a paragraph</p>;
-}
-
 const root = ReactDOM.createRoot(document.querySelector("#maincontent"));
 root.render(<App />);
 
